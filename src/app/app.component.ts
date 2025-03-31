732a0ec6c051edc7aa07aa6fbba732cd68428957
Miguel Gondan <mike@mikesite.es>
2025-04-01 01:56:16 +0200

    implement AWS Amplify client setup and fetch posts in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { generateClient } from 'aws-amplify/data'
+import type { Schema } from '../../amplify/data/resource'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prueba-amp';
+  client;
+  posts: any[] = [];
+
+  constructor() {
+    this.client = generateClient<Schema>();
+  }
+  ngOnInit() {
+    this.client.models.Post.list().then((data) => {
+      this.posts = data.data;
+    });
+  }
 }
